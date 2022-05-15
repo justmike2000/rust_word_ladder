87a9667e632533506cd63c129d1e708e0190d618
Michael Mileusnich <michaelmileusnich@Michaels-MacBook-Air.local>
2022-05-14 23:49:18 -0500

    Linked Hash Set

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use linked_hash_set::LinkedHashSet;
 use std::collections::VecDeque;
 use std::iter::Iterator;
 
@@ -19,8 +19,8 @@ fn diff_one_shot(a: &str, b: &str) -> bool {
 }
 
 // Iters words we are matching on
-fn find_next_words(node: &str, words: Vec<&str>) -> BTreeSet<String> {
-    let mut v = BTreeSet::new();
+fn find_next_words(node: &str, words: LinkedHashSet<String>) -> LinkedHashSet<String> {
+    let mut v = LinkedHashSet::new();
     for word in words.iter() {
         let m = diff_one_shot(node, word);
         if m {
@@ -31,31 +31,39 @@ fn find_next_words(node: &str, words: Vec<&str>) -> BTreeSet<String> {
 }
 
 // Find our word ladder of begin word to end word of one char diff.  Return Vec Solution.
-fn find_word_ladder(begin_word: &str, end_word: &str, word_list: Vec<&str>) -> Vec<String> {
-    // Vec to BTreeSet
-    let mut word_list_set = BTreeSet::new();
-    for word in word_list.clone() {
+fn find_word_ladder(
+    begin_word: &str,
+    end_word: &str,
+    word_list: &mut [&str],
+) -> LinkedHashSet<String> {
+    let mut word_list_set = LinkedHashSet::new();
+    // Vec to LinkedHashSet
+    for word in word_list.iter() {
+        println!("{}", word);
         word_list_set.insert(word.to_string());
     }
 
     // Build a Dequeue to store our nodes for BFS
-    let mut node_list: VecDeque<(String, BTreeSet<String>)> =
-        VecDeque::from([(begin_word.to_string(), word_list_set)]);
+    let mut node_list: VecDeque<(String, LinkedHashSet<String>)> =
+        VecDeque::from([(begin_word.to_string(), word_list_set.clone())]);
 
     // Go huntin'
     while !node_list.is_empty() {
         // Grab left most node
         if let Some((node, path)) = node_list.pop_front() {
             // Find words minus paths we already found
-            let node_next_words = find_next_words(&node, word_list.to_vec());
-            let next_words: BTreeSet<String> = (&path - &node_next_words).iter().cloned().collect();
+            let node_next_words = find_next_words(&node, word_list_set.clone());
+            let next_words: LinkedHashSet<String> =
+                (&path - &node_next_words).iter().cloned().collect();
             // Iter those words
             for next in next_words.iter() {
                 // Solution??
                 if next == end_word {
-                    let mut solution = vec![begin_word.to_string()];
+                    println!("{:?}", path);
+                    let mut solution = LinkedHashSet::new();
+                    solution.insert(begin_word.to_string());
                     for p in path {
-                        solution.push(p);
+                        solution.insert(p);
                     }
                     return solution;
                 // No?  Add the next nodes for huntin'
@@ -68,14 +76,14 @@ fn find_word_ladder(begin_word: &str, end_word: &str, word_list: Vec<&str>) -> V
             }
         }
     }
-    vec![]
+    LinkedHashSet::new()
 }
 
 fn main() {
     let begin_word = "hit";
     let end_word = "cog";
-    let word_list = ["hot", "dot", "dog", "lot", "log", "cog"];
-    let word_ladder = find_word_ladder(begin_word, end_word, word_list.to_vec());
+    let mut word_list = ["hot", "dot", "dog", "lot", "log", "cog"];
+    let word_ladder = find_word_ladder(begin_word, end_word, &mut word_list);
     println!("Begin Word: {} End Word: {}", begin_word, end_word);
     println!("Solution: {:?}", word_ladder);
 }
