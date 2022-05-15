a4d1503f0c59f255b686b3d62bb039cb29a29fb0
Michael Mileusnich <michaelmileusnich@Michaels-MacBook-Air.local>
2022-05-14 22:11:29 -0500

    First commit

---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::iter::Iterator;
+
+// See if &str fidd by one character
+fn diff_one_shot(a: &str, b: &str) -> bool {
+    if a.len() != b.len() {
+        false
+    } else {
+        let mut counter = 0;
+        for i in 0..a.len() {
+            let diff = a.chars().nth(i).unwrap() == b.chars().nth(i).unwrap();
+            if diff {
+                counter += 1;
+            }
+        }
+        counter == 2
+    }
+}
+
+// Iters words we are matching on
+fn find_next_words(node: &str, words: Vec<&str>) -> HashSet<String> {
+    let mut v = HashSet::new();
+    for word in words.iter() {
+        let m = diff_one_shot(node, word);
+        if m {
+            v.insert(word.to_string());
+        }
+    }
+    v
+}
+
+// Find our word ladder of begin word to end word of one char diff.  Return Vec Solution.
+fn find_word_ladder(begin_word: &str, end_word: &str, word_list: Vec<&str>) -> Vec<String> {
+    // Turn our word list into hashset
+    let word_list_string_hashset: HashSet<String> =
+        word_list.iter().map(|s| s.to_string()).collect();
+
+    // Build a Dequeue to store our nodes for BFS
+    let mut node_list: VecDeque<(String, HashSet<String>)> =
+        VecDeque::from([(begin_word.to_string(), word_list_string_hashset)]);
+
+    // Go huntin'
+    while !node_list.is_empty() {
+        // Grab left most node
+        if let Some((node, path)) = node_list.pop_front() {
+            // Find words minus paths we already found
+            let node_next_words = find_next_words(&node, word_list.to_vec());
+            let next_words: Vec<String> = (&path - &node_next_words).iter().cloned().collect();
+            // Iter those words
+            for next in next_words.iter() {
+                // Solution??
+                if next == end_word {
+                    let mut solution = vec![begin_word.to_string()];
+                    for p in path {
+                        solution.push(p);
+                    }
+                    solution.push(end_word.to_string());
+                    return solution;
+                // No?  Add the next nodes for huntin'
+                } else {
+                    let mut new_path = path.clone();
+                    new_path.insert(next.clone());
+                    let mut new_node_list: VecDeque<_> = [(next.clone(), new_path)].into();
+                    node_list.append(&mut new_node_list);
+                }
+            }
+        }
+    }
+    vec![]
+}
+
+fn main() {
+    let begin_word = "hit";
+    let end_word = "cog";
+    let word_list = ["hot", "dot", "dog", "lot", "log", "cog"];
+    let word_ladder = find_word_ladder(begin_word, end_word, word_list.to_vec());
+    println!("Begin Word: {} End Word: {}", begin_word, end_word);
+    println!("Solution: {:?}", word_ladder);
+}
